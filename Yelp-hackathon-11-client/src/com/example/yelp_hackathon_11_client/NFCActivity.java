b50787851b29512283f9703fc21b33e0d9c3d739
Jonathan Maltz <maltz@yelp.com>
2013-07-25 12:56:20 -0700

    fuck everything about tabs and spaces problems

--- Yelp-hackathon-11-client/src/com/example/yelp_hackathon_11_client/NFCActivity.java
+++ Yelp-hackathon-11-client/src/com/example/yelp_hackathon_11_client/NFCActivity.java
@@ -12,53 +12,57 @@ import android.view.Menu;
 import android.widget.TextView;
 
 public class NFCActivity extends Activity {
-	
-	String mText;
+
+	int mTagId;
 	TextView mTextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nfc);
-        
-        mTextView = (TextView) findViewById(R.id.activity_nfc_text);
-        
-    }
-    
-    protected void onResume() {
-    	if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-    		Parcelable[] rawMsgs = getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-    		Log.e("MALTZ", "we have " + rawMsgs.length + " messages");
-    		NdefMessage msg = (NdefMessage) rawMsgs[0];
-    		
-    		byte[] payload = msg.getRecords()[0].getPayload();
-    		
-            // Code shamelessly stolen from stack overflow
-    		
-    		//Get the Text Encoding
-            String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
-            
-            //Get the Language Code
-            int languageCodeLength = payload[0] & 0077;
-           
-            try {
-	            //Get the Text
-	            mText = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
-	            mTextView.setText(mText);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_nfc);
+
+		mTextView = (TextView) findViewById(R.id.activity_nfc_text);
+	}
+
+	protected void onResume() {
+		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+			Parcelable[] rawMsgs = getIntent().getParcelableArrayExtra(
+					NfcAdapter.EXTRA_NDEF_MESSAGES);
+			Log.e("MALTZ", "we have " + rawMsgs.length + " messages");
+			NdefMessage msg = (NdefMessage) rawMsgs[0];
+
+			byte[] payload = msg.getRecords()[0].getPayload();
+
+			// Code shamelessly stolen from stack overflow
+
+			// Get the Text Encoding
+			
+			String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8"
+					: "UTF-16";
+
+			// Get the Language Code
+			int languageCodeLength = payload[0] & 0077;
+
+			try {
+				// Get the Text
+				String text = new String(payload, languageCodeLength + 1,
+						payload.length - languageCodeLength - 1, textEncoding);
+				String id = text.substring(text.lastIndexOf(":") + 1);
+				mTagId = Integer.parseInt(id);
+				mTextView.setText(String.valueOf(mTagId));
 			} catch (UnsupportedEncodingException e) {
 				Log.e("MALTZ", "seriously??? you don't have UTF-8 or UTF-16??");
 			}
-    	}
+		}
 
-    	super.onResume();
-    }
+		super.onResume();
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.nfc, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nfc, menu);
-        return true;
-    }
-    
 }
