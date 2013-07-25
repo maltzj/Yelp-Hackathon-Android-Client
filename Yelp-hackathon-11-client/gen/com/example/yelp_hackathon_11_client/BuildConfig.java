1090eb11ccd16c3045b9b7b71c6aee47625220b0
Jonathan Maltz <maltz@yelp.com>
2013-07-25 11:36:12 -0700

    adding gen, because I'm bad

---
+++ Yelp-hackathon-11-client/gen/com/example/yelp_hackathon_11_client/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.yelp_hackathon_11_client;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
